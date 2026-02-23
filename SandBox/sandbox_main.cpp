732633396fadb38d643a712377424fe2a3aa87f1
vbaswin <aswinvb10a@gmail.com>
2026-02-23 18:18:10 +0530

    wip(smartvolmapper): placing the base function

--- SandBox/sandbox_main.cpp
+++ SandBox/sandbox_main.cpp
@@ -1,6 +1,19 @@
+#include <QDebug>
+#include "vtkDICOMReader.h"
+#include "vtkGPUVolumeRayCastMapper.h"
+#include "vtkImageData.h"
+#include "vtkSmartPointer.h"
 #include <iostream>
+
+const char *filefname = "C:/Users/cdac/Projects/SE2dcm";
+
+vtkImageData *getImageData()
+{
+    vtkDICOMReader *reader = vtkSmartPointer<vtkDICOMReader>::New();
+    return reader->GetOutput();
+}
 
 int main()
 {
-    std::cout << "hello";
+    // qDebug() << "hello";
 }
